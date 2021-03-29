9a70c660d3441dd4e831e81bb1489c2134ac2744
TomWang <907465687@qq.com>
2021-03-29 22:27:09 +0800

    add MVC project

---
+++ IOCDemo/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace IOCDemo.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
